36025665db1aa351b868c62c0f065bb6a3c02275
AbdAlrhman Mohamed <abdooamino0@gmail.com>
2024-07-30 23:38:47 +0300

    upload For Test

--- src/Components/MainNav/MainNav.jsx
+++ src/Components/MainNav/MainNav.jsx
@@ -1,7 +1,25 @@
 import React from 'react'
 
 export default function MainNav() {
-  return (
-    <div>MainNav</div>
-  )
+    return (
+        <div>
+            <nav className='fixed top-0 left-0 right-0 p-2 bg-white border border-slate-300 border-opacity-55 border-t-0 z-40 flex items-center justify-evenly'>
+                <div className='relative'>
+                    <input className='w-96 h-8 p-5 px-7 border-2 border-slate-300 rounded-2xl bg-slate-50' type="text" placeholder='Search...' />
+                    <i className="fa-solid text-slate-400 fa-magnifying-glass absolute left-2 top-1/2 -translate-y-1/2"></i>
+                </div>
+                <ul className='flex gap-x-10 text-slate-500'>
+                    <li><i className="fa-solid fa-inbox mr-2 text-lg"></i> inbox</li>
+                    <li><i className="fa-solid fa-comments mr-2 text-lg"></i> Chat</li>
+                    <li><i className="fa-solid fa-gear fa-spin mr-2 text-lg"></i> Setting</li>
+                </ul>
+                <ul>
+                    <li><i className="fa-regular fa-bell"></i></li>
+                </ul>
+                <div className='xl:hidden cursor-pointer'>
+                    <i className="fa-solid fa-sliders text-2xl"></i>
+                </div>
+            </nav>
+        </div>
+    )
 }
--- src/Components/SideNav/SideNav.jsx
+++ src/Components/SideNav/SideNav.jsx
@@ -1,7 +1,54 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react';
+
 
 export default function SideNav() {
-  return (
-    <div>SideNav</div>
-  )
-}
+    const [openNav, setOpenNav] = useState(false);
+    const [barwidth, setBarWidth] = useState();
+    let test = useRef()
+    console.log(barwidth);
+
+    function toggle() {
+        setOpenNav(!openNav);
+    };
+
+    useEffect(() => {
+        console.log(test);
+        setBarWidth(test.current.scrollWidth)
+    }, [])
+
+    return (
+        <nav ref={test} className={`flex flex-col z-50 bg-white shadow-md text-slate-700 border-r-2 w-64 h-full fixed top-0 left-0 bottom-0 transform ${openNav ? 'translate-x-0' : '-translate-x-3/4'} transition-transform duration-300`}>
+            <button className="p-6 self-end hover:scale-110 duration-300" onClick={() => toggle()}>
+                {openNav ? <i className="fa-solid fa-angles-left"></i> : <i className="fa-solid fa-angles-right"></i>}
+            </button>
+            <div className='bg-slate-300 w-3/4 h-px absolute top-9 -translate-y-1/2'></div>
+            {openNav ? <ul className="mt-10 cursor-pointer overflow-hidden">
+                <li className="p-6 hover:bg-gray-300 border-b-2 border-slate-700 border-opacity-5 hover:scale-110 duration-300">
+                    <i className="fas fa-home mr-5"></i> Home
+                </li>
+                <li className="p-6 hover:bg-gray-300 border-b-2 border-slate-700 border-opacity-5 hover:scale-110 duration-300">
+                    <i className="fa-solid fa-address-card mr-5"></i> About
+                </li>
+                <li className="p-6 hover:bg-gray-300 border-b-2 border-slate-700 border-opacity-5 hover:scale-110 duration-300">
+                    <i className="fa-brands fa-servicestack mr-5"></i> Services
+                </li>
+                <li className="p-6 hover:bg-gray-300 hover:scale-110 duration-300">
+                    <i className="fa-solid fa-address-book mr-5"></i> Contact
+                </li>
+            </ul > : <ul className='flex flex-col items-end cursor-pointer mt-10 overflow-hidden'>
+                <li className="p-6 hover:bg-gray-300 border-b-2 border-slate-700 border-opacity-5 hover:scale-110 duration-300">
+                    <i className="fas fa-home"></i>
+                </li>
+                <li className="p-6 hover:bg-gray-300 border-b-2 border-slate-700 border-opacity-5 hover:scale-110 duration-300">
+                    <i className="fa-solid fa-address-card"></i>
+                </li>
+                <li className="p-6 hover:bg-gray-300 border-b-2 border-slate-700 border-opacity-5 hover:scale-110 duration-300">
+                    <i className="fa-brands fa-servicestack"></i>
+                </li>
+                <li className="p-6 hover:bg-gray-300 hover:scale-110 duration-300">
+                    <i className="fa-solid fa-address-book"></i>
+                </li>
+            </ul>}
+        </nav >
+    );
+};
