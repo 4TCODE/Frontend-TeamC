7d6f06b1fe78e26b6490f4829f3550eb28f635a4
Marwa Mohammed <52058731+MarwaElasuty@users.noreply.github.com>
2024-08-01 23:21:15 +0300

    updated-style

--- src/Components/Table/TableCell.jsx
+++ src/Components/Table/TableCell.jsx
@@ -5,8 +5,8 @@ const TableCell = ({ children}) => {
     return (
         <div>
             <td className='px-4 py-2'>
-                <div className='flex items-center'>
-                    <img src={abstractImage} alt="abstract" className="sm:w-1/2 md:w-1/3" />
+                <div className='flex items-center md:flex-col sm:flex-col'>
+                    <img src={abstractImage} alt="abstract" className="sm:w-1/2 md:w-1/4" />
                     <p>{children}</p>
                 </div>
             </td>
